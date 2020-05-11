2813eb06ae5358c1ac4398dfb47f10b04b71417a
lei-hsia <lx2196@columbia.edu>
2020-05-11 16:01:40 +0800

    最简单的consumer

---
+++ kafka-demo/src/main/java/com/lei/consumer/MyConsumer.java
@@ -0,0 +1,43 @@
+package com.lei.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class MyConsumer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 连接的集群
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092");
+        // 开启自动提交
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        // 自动提交的interval
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+        // key value反序列化
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserialization");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserialization");
+        // 消费者组
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka01");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // 订阅主题
+        consumer.subscribe(Arrays.asList("first","second"));
+
+        while (true) {
+            // 拉取数据
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
+            // 解析并打印，遍历
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.key()+consumerRecord.value()); //consumer能得到key,说明key是存到了本地的，否则获取不到的
+            }
+        }
+
+        // consumer.close();
+    }
+}
