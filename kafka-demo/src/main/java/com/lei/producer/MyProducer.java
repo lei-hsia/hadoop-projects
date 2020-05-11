31f6b32931e81f8436ace39a9789385c8902f7db
lei-hsia <lx2196@columbia.edu>
2020-05-11 15:00:50 +0800

    Ordinary Producer

---
+++ kafka-demo/src/main/java/com/lei/producer/MyProducer.java
@@ -0,0 +1,35 @@
+package com.lei.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class MyProducer {
+    public static void main(String[] args) {
+
+        // 1. config
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092");
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        // default
+        properties.put(ProducerConfig.RETRIES_CONFIG, "1");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); // RecordAccumulator
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // 2. 发送消息
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("first_topic", "atguigu--"+i));
+        }
+
+        // 3. 关闭资源
+        producer.close(); // 阻塞main线程，使得sender线程
+    }
+}
