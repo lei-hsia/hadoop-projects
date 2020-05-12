6963a12bc727549bfea2b691b9ccfe3c016cdb12
lei-hsia <lx2196@columbia.edu>
2020-05-12 17:09:13 +0800

    CounterInterceptor

--- kafka-demo/src/main/java/com/lei/interceptor/CounterInterceptor.java
+++ kafka-demo/src/main/java/com/lei/interceptor/CounterInterceptor.java
@@ -7,6 +7,10 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import java.util.Map;
 
 public class CounterInterceptor implements ProducerInterceptor<String, String> {
+
+    private int success;
+    private int error;
+
     @Override
     public void configure(Map<String, ?> map) {
 
@@ -19,12 +23,15 @@ public class CounterInterceptor implements ProducerInterceptor<String, String> {
 
     @Override
     public void onAcknowledgement(RecordMetadata recordMetadata, Exception e) {
-
+        if (recordMetadata != null) {
+            success++;
+        } else error++;
     }
 
     @Override
     public void close() {
-
+        System.out.println("success: "+success);
+        System.out.println("error: "+error);
     }
 
 }
