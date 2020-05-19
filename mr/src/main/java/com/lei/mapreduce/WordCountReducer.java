e7255a1c77f443c10f2a82f1c92ed1b0a1625692
lei-hsia <lx2196@columbia.edu>
2020-05-19 16:17:08 +0800

    WordCountReducer

---
+++ mr/src/main/java/com/lei/mapreduce/WordCountReducer.java
@@ -0,0 +1,25 @@
+package com.lei.mapreduce;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    // <atguigu,1>  <atguigu,1> --> <atguigu,2>
+
+    int sum = 0;
+    IntWritable v = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        for (IntWritable v : values) {
+            sum += v.get();
+        }
+
+        v.set(sum);
+        context.write(key,v);
+    }
+}
