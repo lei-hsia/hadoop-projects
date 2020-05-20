c68e8f1773ecf9982d848f47909fc4892677c97e
lei-hsia <lx2196@columbia.edu>
2020-05-20 14:42:14 +0800

    WordCountDriver

---
+++ mr/src/main/java/com/lei/mapreduce/WordCountDriver.java
@@ -0,0 +1,41 @@
+package com.lei.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        // 1 创建job对象
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        // 2. 设置jar路径: 本类运行时需要的jar路径:通过反射找到
+        job.setJarByClass(WordCountDriver.class);
+        // 3. 关联mapper, reducer类
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        // 4. 设置mapper和最终输出KV类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 5. 设置输入输出路径
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 6. 多线程的方法
+        boolean b = job.waitForCompletion(true);
+
+        System.exit(b ? 0 : 1);
+    }
+}
--- mr/src/main/java/com/lei/mapreduce/WordCountReducer.java
+++ mr/src/main/java/com/lei/mapreduce/WordCountReducer.java
@@ -9,12 +9,12 @@ import java.io.IOException;
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     // <atguigu,1>  <atguigu,1> --> <atguigu,2>
 
-    int sum = 0;
     IntWritable v = new IntWritable();
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
+        int sum = 0;
         for (IntWritable v : values) {
             sum += v.get();
         }
